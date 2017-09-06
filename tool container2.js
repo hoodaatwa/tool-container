f048f0f965e9d5f05a1eeb083e2172bb8710c4a2
hoodaatwa <31570775+hoodaatwa@users.noreply.github.com>
2017-09-06 17:07:44 +0200

    Create tool container2.js

---
+++ tool container2.js
@@ -0,0 +1,7 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+	 
+
+	<script src="ws-lightbox.js" type="text/javascript"></script>
+
+	<link type="text/css" rel="stylesheet" href="ws-lightbox.css">
